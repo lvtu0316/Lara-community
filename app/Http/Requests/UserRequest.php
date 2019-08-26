34fdbd669fea034749c3325d02623560ab8a584c
lvtu0316 <550407948@qq.com>
2019-08-26 09:41:03 +0800

    上传头像限制大小

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,7 +28,7 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
-            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=200,min_height=200',
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=200,min_height=200|size:200',
         ];
     }
 
@@ -41,6 +41,7 @@ class UserRequest extends FormRequest
             'name.required' => '用户名不能为空。',
             'avatar.mimes' =>'头像必须是 jpeg, bmp, png, gif 格式的图片',
             'avatar.dimensions' => '图片的清晰度不够，宽和高需要 200px 以上',
+            'avatar.size' => '图片不能超过2M',
         ];
     }
 }
